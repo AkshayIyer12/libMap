4526abd9697977dbe99da16c79d9636ceac00974
Akshay Iyer <akshayi@versa-networks.com>
2018-03-16 11:54:50 +0530

    Add conditional rendering of list and Empty list

--- src/BookList.js
+++ src/BookList.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 class BookList extends Component {
   render () {
     let books = [this.props.books]
-    console.log(books)
-    return (<ul>
-      {books.map(book => (
-        <div key={book.id}>
-          <li>{book.title} | {book.author}</li>
-        </div>
-      ))}
-    </ul>)
+    if (books.length > 0 && !books.includes(undefined)) {
+      return (<ul>
+        {books.map(book => (
+          <div key={book.id}>
+            <li>{book.title} | {book.author}</li>
+          </div>
+        ))}
+      </ul>)
+    } else {
+      return (<p>No Entry to show</p>)
+    }
   }
 }
 
